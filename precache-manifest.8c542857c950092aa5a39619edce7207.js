575d1223c5d1efe0cf227ad29f97b675cde8755d
Aman Shrestha <ashrest2@u.rochester.edu>
2021-02-22 23:16:48 -0500

    Updates

---
+++ precache-manifest.8c542857c950092aa5a39619edce7207.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef9c64a1cfce82ae6b2333252c1f1987",
+    "url": "/Careers-Uncorked-new/index.html"
+  },
+  {
+    "revision": "fa4e9113ddd0d9c8cd76",
+    "url": "/Careers-Uncorked-new/static/css/main.02571210.chunk.css"
+  },
+  {
+    "revision": "0c2bddcea31f290d8296",
+    "url": "/Careers-Uncorked-new/static/js/2.e01542f2.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Careers-Uncorked-new/static/js/2.e01542f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa4e9113ddd0d9c8cd76",
+    "url": "/Careers-Uncorked-new/static/js/main.79d2270b.chunk.js"
+  },
+  {
+    "revision": "4b6f6f3d5038f5bdd451",
+    "url": "/Careers-Uncorked-new/static/js/runtime-main.9f7a8cd2.js"
+  },
+  {
+    "revision": "3b24b1f30eb3f50be6690bfca73f609d",
+    "url": "/Careers-Uncorked-new/static/media/Daniel.3b24b1f3.png"
+  },
+  {
+    "revision": "a596e16efddd7b7b3abf4311ed88ad2f",
+    "url": "/Careers-Uncorked-new/static/media/MailImage.a596e16e.png"
+  },
+  {
+    "revision": "1ae947518e3f52d90afe9f804c0905d3",
+    "url": "/Careers-Uncorked-new/static/media/Michelle_pic.1ae94751.png"
+  },
+  {
+    "revision": "4e44b231c87df4578fba501fb69ea1d3",
+    "url": "/Careers-Uncorked-new/static/media/lightBlueBackground.4e44b231.png"
+  },
+  {
+    "revision": "48595e857fe22fc7f760bb1fa62a0c6b",
+    "url": "/Careers-Uncorked-new/static/media/mainpage-back-drop.48595e85.png"
+  },
+  {
+    "revision": "33e167265b53ae89d807b648025e0562",
+    "url": "/Careers-Uncorked-new/static/media/mainpage-hangout.33e16726.png"
+  },
+  {
+    "revision": "db08dd2ab2ae73caeca1ee08a0cae01c",
+    "url": "/Careers-Uncorked-new/static/media/podcastBackground.db08dd2a.png"
+  },
+  {
+    "revision": "2a51fac6da0ce00ebfc0368ce9fef0ed",
+    "url": "/Careers-Uncorked-new/static/media/purpleBackground.2a51fac6.png"
+  },
+  {
+    "revision": "61ad48d9e2467aa81b8508eb3705f534",
+    "url": "/Careers-Uncorked-new/static/media/sharfuz.61ad48d9.png"
+  },
+  {
+    "revision": "51a28c593f772b91ce69f3688e362f39",
+    "url": "/Careers-Uncorked-new/static/media/sid.51a28c59.png"
+  },
+  {
+    "revision": "8963e4d3699d5142ca1d530625dd3373",
+    "url": "/Careers-Uncorked-new/static/media/uncorkedLogo.8963e4d3.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2c1e7a03152e0b78fa91f736392922ad.js"
+  "/Careers-Uncorked-new/precache-manifest.8c542857c950092aa5a39619edce7207.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Careers-Uncorked-new/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
